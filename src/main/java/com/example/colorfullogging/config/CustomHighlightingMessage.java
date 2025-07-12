0a55337e96a3f05e83306f7e18552597e19c89e6
Viktor Gamov <gamussa@gmail.com>
2025-07-11 22:27:15 -0400

    minor refactoring + fixing log duplicates

--- src/main/java/com/example/colorfullogging/config/CustomHighlightingMessage.java
+++ src/main/java/com/example/colorfullogging/config/CustomHighlightingMessage.java
@@ -1,31 +1,8 @@
 package com.example.colorfullogging.config;
 
-import ch.qos.logback.classic.Level;
-import ch.qos.logback.classic.spi.ILoggingEvent;
-import ch.qos.logback.core.pattern.color.ANSIConstants;
-import ch.qos.logback.core.pattern.color.ForegroundCompositeConverterBase;
-
 /**
  * Custom converter for colorizing log messages in console output
  */
-public class CustomHighlightingMessage extends ForegroundCompositeConverterBase<ILoggingEvent> {
-    
-    @Override
-    protected String getForegroundColorCode(ILoggingEvent event) {
-        Level level = event.getLevel();
-        switch (level.toInt()) {
-            case Level.ERROR_INT:
-                return ANSIConstants.RED_FG;
-            case Level.WARN_INT:
-                return ANSIConstants.YELLOW_FG;
-            case Level.INFO_INT:
-                return ANSIConstants.GREEN_FG;
-            case Level.DEBUG_INT:
-                return ANSIConstants.BLUE_FG;
-            case Level.TRACE_INT:
-                return ANSIConstants.MAGENTA_FG;
-            default:
-                return ANSIConstants.DEFAULT_FG;
-        }
-    }
+public class CustomHighlightingMessage extends BaseColorConverter {
+    // Uses the default color mapping from BaseColorConverter
 }
--- src/main/java/com/example/colorfullogging/config/CustomHighlightingPackageName.java
+++ src/main/java/com/example/colorfullogging/config/CustomHighlightingPackageName.java
@@ -1,29 +1,23 @@
 package com.example.colorfullogging.config;
 
 import ch.qos.logback.classic.Level;
-import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.pattern.color.ANSIConstants;
-import ch.qos.logback.core.pattern.color.ForegroundCompositeConverterBase;
 
 /**
  * Custom converter for colorizing package names in console output
  */
-public class CustomHighlightingPackageName extends ForegroundCompositeConverterBase<ILoggingEvent> {
+public class CustomHighlightingPackageName extends BaseColorConverter {
     
     @Override
-    protected String getForegroundColorCode(ILoggingEvent event) {
-        Level level = event.getLevel();
+    protected String getColorForLevel(Level level) {
         switch (level.toInt()) {
             case Level.ERROR_INT:
                 return ANSIConstants.RED_FG;
             case Level.WARN_INT:
                 return ANSIConstants.YELLOW_FG;
             case Level.INFO_INT:
-                return ANSIConstants.CYAN_FG;
             case Level.DEBUG_INT:
-                return ANSIConstants.CYAN_FG;
             case Level.TRACE_INT:
-                return ANSIConstants.CYAN_FG;
             default:
                 return ANSIConstants.CYAN_FG;
         }
